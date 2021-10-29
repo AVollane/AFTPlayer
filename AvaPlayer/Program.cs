c8805006013e47be6e8181a6b4ae2d067f9416be
Vollane <AntWorkingV@outlook.com>
2021-10-29 14:43:23 +0300

    Добавлено скачивание файлов с указанного сервера при запуске плеера
    - Добавлена библиотека для работы с сетью и протоколом SFTP
    - Если подключение к серверу прошло успешно, то производится очистка папки с видеофайлами и скачивание новых
    - В конфигурацию добавлены новые пункты
    - Исправлен баг с выдачей неверного индекса медиа

--- AvaPlayer/Program.cs
+++ AvaPlayer/Program.cs
@@ -1,17 +1,96 @@
+using AFTClient;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
 using System;
+using System.Configuration;
+using System.IO;
+using System.Threading;
 
 namespace AvaPlayer
 {
     class Program
     {
+        private static Receiver receiver;
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args) 
+        {
+            string? ftpHostIP = ConfigurationManager.AppSettings.Get("FTPHostIP");
+            string? ftpUsername = ConfigurationManager.AppSettings.Get("FTPUsername");
+            string? ftpPassword = ConfigurationManager.AppSettings.Get("FTPPassword");
+            receiver = new Receiver(ftpHostIP, ftpUsername, ftpPassword);
+
+            int recurrences;
+            string? recurrencesStr = ConfigurationManager.AppSettings.Get("ConnectionsRecurrences");
+            if(Int32.TryParse(recurrencesStr, out recurrences))
+            {
+                recurrences = 5;
+            }
+
+            int millisecondsDelay;
+            string? millisecondsDelayStr = ConfigurationManager.AppSettings.Get("ConnectionMillisecondsDelay");
+            if(Int32.TryParse(millisecondsDelayStr, out millisecondsDelay))
+            {
+                millisecondsDelay = 5000;
+            }
+            TryConnect(recurrences, millisecondsDelay);
+
+            if (receiver.IsConnected)
+            {
+                string? remoteDirectory = ConfigurationManager.AppSettings.Get("RemoteDirectory");
+                string? localDirectory = ConfigurationManager.AppSettings.Get("LocalDirectory");
+                RemoveOldFiles(localDirectory);
+                receiver.DownloadFiles(remoteDirectory, localDirectory);
+            }
+
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        private static void TryConnect(int recurrences, int millisecondsDelay)
+        {
+            if (recurrences == 0)
+                recurrences = 5;
+            if (millisecondsDelay == 0)
+                millisecondsDelay = 5000;
+
+            for (int i = 0; i < recurrences; i++)
+            {
+                try
+                {
+                    receiver.Connect();
+                    break;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(millisecondsDelay);
+                }
+            }
+        }
+
+        private static void RemoveOldFiles(string? localDirectory)
+        {
+            try
+            {
+                string[] filesInLocalDir = Directory.GetFiles(localDirectory);
+                foreach (string file in filesInLocalDir)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
---
+++ FileReceiverLib/Receiver.cs
@@ -0,0 +1,81 @@
+﻿using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFTClient
+{
+    public class Receiver
+    {
+        private SftpClient _client;
+        private string _host;
+        private string _username;
+        private string _password;
+
+        public Receiver(string host, string username, string password)
+        {
+            _host = host;
+            _username = username;
+            _password = password;
+            _client = new SftpClient(_host, _username, _password);
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _client.IsConnected;
+            }
+        }
+
+        /// <summary>
+        /// Подключается к удаленному SFTP-серверу
+        /// </summary>
+        public void Connect() => _client.Connect();
+
+        /// <summary>
+        /// Отсоединяемся от удаленного SFTP-сервера
+        /// </summary>
+        public void Disconnect() => _client.Disconnect();
+
+        /// <summary>
+        /// Скачивает все файлы из удаленной директории в локальную директорию
+        /// </summary>
+        /// <param name="remoteDirectory">Удаленная директория</param>
+        /// <param name="localDirectory">Локальная директория</param>
+        public void DownloadFiles(string remoteDirectory, string localDirectory)
+        {
+            if (String.IsNullOrEmpty(remoteDirectory) && String.IsNullOrEmpty(localDirectory))
+                return;
+
+            if (localDirectory.EndsWith('/'))
+                localDirectory += @"/";
+            if (remoteDirectory.EndsWith('/'))
+                remoteDirectory += @"/";
+
+            // Файлы, которые начинаются с точки, являются системными. Мы не должны их скачивать,
+            // по этому фильтруем названия
+            IEnumerable<SftpFile> files = _client.ListDirectory(remoteDirectory).Where(x => !x.Name.StartsWith('.'));
+
+            foreach(SftpFile file in files)
+            {
+                try
+                {
+                    using (FileStream fileStream = new FileStream($"{localDirectory}/{file.Name}", FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        _client.DownloadFile($"{remoteDirectory}/{file.Name}", fileStream);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
